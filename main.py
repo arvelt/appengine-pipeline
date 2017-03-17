0c3b33cd5e29104a67f58b38bfe0d1d1d4d4a704
Keisuke Ota <arvelt.s@gmail.com>
2017-03-17 19:12:29 +0900

    add taskqueue

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 import logging
 import pipeline
+from google.appengine.ext import deferred
 from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def index():
+    return """
+    <li><a href="/test-deferred"/>/test-deferred</li>
+    <li><a href="/pypeline-sync">/pypeline-sync</li>
+    """
+
+def do_something_expensive(a, b, c=None):
+    logging.info("Doing something expensive! %r %r %r", a, b, c)
+
+@app.route("/test-deferred")
+def test_deferred():
+    deferred.defer(do_something_expensive, "Hello, world!", 42, c=True)
+    return "See server log."
 
 @app.route("/pypeline-sync")
-def get():
+def pypeline_sync():
     square_stage = SquarePipeline(10)
     square_stage.start()
-    return ""
+    return "See server log"
 
 class SquarePipeline(pipeline.Pipeline):
 
