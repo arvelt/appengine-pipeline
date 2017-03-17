b2b2b743f6e332a41c44ed2a35d5c5bbe746a18c
Keisuke Ota <arvelt.s@gmail.com>
2017-03-17 19:21:37 +0900

    add multiple tasks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+# -*- coding: utf-8 -*-
 import logging
 import pipeline
-from google.appengine.ext import deferred
 from flask import Flask
 app = Flask(__name__)
 
@@ -9,21 +9,29 @@ def index():
     return """
     <li><a href="/test-deferred"/>/test-deferred</li>
     <li><a href="/pypeline-sync">/pypeline-sync</li>
+    <li><a href="/pypeline-multiple-tasks">/pypeline-multiple-tasks</li>
     """
+
 
 def do_something_expensive(a, b, c=None):
     logging.info("Doing something expensive! %r %r %r", a, b, c)
 
+
 @app.route("/test-deferred")
 def test_deferred():
+    u"""Use deferred."""
+    from google.appengine.ext import deferred
     deferred.defer(do_something_expensive, "Hello, world!", 42, c=True)
     return "See server log."
 
+
 @app.route("/pypeline-sync")
 def pypeline_sync():
+    """Use pypeline sync."""
     square_stage = SquarePipeline(10)
     square_stage.start()
     return "See server log"
+
 
 class SquarePipeline(pipeline.Pipeline):
 
@@ -34,6 +42,40 @@ class SquarePipeline(pipeline.Pipeline):
 
     def finalized(self):
         logging.info('All done! Square is %s', self.outputs.square.value)
+
+
+@app.route("/pypeline-multiple-tasks")
+def pypeline_multiple_tasks():
+    stage = FanOutFanInPipeline(10)
+    stage.start()
+    return "See server log"
+
+
+class SquarePipeline2(pipeline.Pipeline):
+
+    def run(self, number):
+        logging.info('Squaring: %s' % number)
+        return number * number
+
+
+class Sum(pipeline.Pipeline):
+
+    def run(self, *args):
+        value = sum(list(args))
+        logging.info('Sum: %s', value)
+        return value
+
+
+class FanOutFanInPipeline(pipeline.Pipeline):
+
+    def run(self, count):
+        results = []
+        for i in xrange(0, count):
+            result = yield SquarePipeline2(i)
+            results.append(result)
+
+        yield Sum(*results)
+
 
 if __name__ == "__main__":
     app.run()
